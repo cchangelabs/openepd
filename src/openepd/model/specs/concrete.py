7ac627e2212419dc9a09a76e2ae328e054c47fcc
Alex Cherednichenko <alex.cherednichenko@c-change-labs.com>
2024-02-14 15:55:44 -0500

    feat: add TypicalApplication spec for Concrete

--- src/openepd/model/specs/concrete.py
+++ src/openepd/model/specs/concrete.py
@@ -74,6 +74,73 @@ class CmuOptions(BaseOpenEpdSpec):
     )
 
 
+class ConcreteTypicalApplication(BaseOpenEpdSpec):
+    """Typical Application for Concrete."""
+
+    fnd: bool | None = pyd.Field(
+        description="Foundation. Typically used in direct contact with soil, e.g. footings, piles, mass concrete, "
+        "mat foundations, and similar applications.",
+        example=True,
+        default=None,
+    )
+    sog: bool | None = pyd.Field(
+        description="Slab on Grade. Typically used in continuously supported horizontal "
+        "applications e.g. slab on grade, topping slabs, sidewalks, and roadways.",
+        example=True,
+        default=None,
+    )
+    hrz: bool | None = pyd.Field(
+        description="Elevated Horizontal. Typically used in elevated horizontal applications, either on metal deck or "
+        "where soffit formwork must be removed, e.g. post-tension plates, rebar plates, beams and slabs, "
+        "waffle slabs.",
+        example=True,
+        default=None,
+    )
+    vrt_wall: bool | None = pyd.Field(description="Vertical Wall.", example=True, default=None)
+    vrt_column: bool | None = pyd.Field(description="Vertical Column.", example=True, default=None)
+    vrt_other: bool | None = pyd.Field(
+        description="Vertical Other. Typically used in vertical applications other than "
+        "walls or columns, e.g. sloped surfaces where formwork is required "
+        "on multiple faces.",
+        example=True,
+        default=None,
+    )
+    sht: bool | None = pyd.Field(
+        description="Shotcrete. Pneumatically applied, without formwork on all sides.", example=True, default=None
+    )
+    cdf: bool | None = pyd.Field(
+        description="Flowable Fill (CDF). Typically used to fill voids, backfill retaining "
+        "walls, as a sub-base, and similar applications. Also called Controlled "
+        "Density Fill (CDF) or Controlled Low Strength Materials (CLSM).",
+        example=True,
+        default=None,
+    )
+    sac: bool | None = pyd.Field(
+        description="Typically used in concrete sidewalks and barrier curbs.", example=True, default=None
+    )
+    pav: bool | None = pyd.Field(description="Typically used in pervious concrete", example=True, default=None)
+    oil: bool | None = pyd.Field(
+        description="Concretes for use in creation, maintenance, and decommissioning of "
+        "petroleum extraction wells and similar applications. Includes foamed "
+        "cement; often called cement in the drilling industry. Differs from "
+        "flowable fill and grout in that it contains no sand or other aggregates.",
+        example=True,
+        default=None,
+    )
+    grt: bool | None = pyd.Field(
+        description="Cement grouting is a slurry that is placed as a flowable liquid. It is "
+        "an effective material for filling and strengthening granular soils, "
+        "voids in rocks, foundation underpinnings, and other underground voids. "
+        "Also called structural grout, these materials typically impart"
+        " significant strength to the system",
+        example=True,
+        default=None,
+    )
+    ota: bool | None = pyd.Field(
+        description="Typical application not covered by other values.", example=True, default=None
+    )
+
+
 class CmuSpec(BaseOpenEpdSpec):
     """Standardized Concrete Masonry Unit-specific extension for OpenEPD."""
 
