4e6fa93128b29409d8f07a5f87600746bee0730b
Alex Cherednichenko <alex.cherednichenko@c-change-labs.com>
2024-03-25 09:12:30 -0400

    feat: add initial scaffolded openepd pydantic models
    asana: [Run scaffolding: tests, pydantic models, adaptors](https://app.asana.com/0/0/1206887306120198/f)

---
+++ src/openepd/model/specs/generated/other_materials.py
@@ -0,0 +1,147 @@
+#
+#  Copyright 2024 by C Change Labs Inc. www.c-change-labs.com
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+#  This software was developed with support from the Skanska USA,
+#  Charles Pankow Foundation, Microsoft Sustainability Fund, Interface, MKA Foundation, and others.
+#  Find out more at www.BuildingTransparency.org
+#
+from openepd.model.specs.base import BaseOpenEpdHierarchicalSpec
+
+UnknownStrTypeHandleMe = str
+
+
+class AuxiliariesV1(BaseOpenEpdHierarchicalSpec):
+    """Auxiliaries performance specification."""
+
+    _EXT_VERSION = "1.0"
+
+    pass
+
+
+class CleaningProductsV1(BaseOpenEpdHierarchicalSpec):
+    """Cleaning products performance specification."""
+
+    _EXT_VERSION = "1.0"
+
+    pass
+
+
+class ClothingV1(BaseOpenEpdHierarchicalSpec):
+    """Clothing performance specification."""
+
+    _EXT_VERSION = "1.0"
+
+    pass
+
+
+class FoodBeverageV1(BaseOpenEpdHierarchicalSpec):
+    """Food beverage performance specification."""
+
+    _EXT_VERSION = "1.0"
+
+    pass
+
+
+class TransportationInfrastructureV1(BaseOpenEpdHierarchicalSpec):
+    """Transportation infrastructure performance specification."""
+
+    _EXT_VERSION = "1.0"
+
+    # Nested specs:
+    Auxiliaries: AuxiliariesV1 | None = None
+
+
+class UnsupportedV1(BaseOpenEpdHierarchicalSpec):
+    """Unsupported performance specification."""
+
+    _EXT_VERSION = "1.0"
+
+    # Nested specs:
+    CleaningProducts: CleaningProductsV1 | None = None
+    Clothing: ClothingV1 | None = None
+    FoodBeverage: FoodBeverageV1 | None = None
+
+
+class CopperV1(BaseOpenEpdHierarchicalSpec):
+    """Copper performance specification."""
+
+    _EXT_VERSION = "1.0"
+
+    pass
+
+
+class EarthworkV1(BaseOpenEpdHierarchicalSpec):
+    """Earthwork performance specification."""
+
+    _EXT_VERSION = "1.0"
+
+    pass
+
+
+class ExteriorSunControlDevicesV1(BaseOpenEpdHierarchicalSpec):
+    """Exterior sun control devices performance specification."""
+
+    _EXT_VERSION = "1.0"
+
+    pass
+
+
+class GypsumFinishingCompoundsV1(BaseOpenEpdHierarchicalSpec):
+    """Gypsum finishing compounds performance specification."""
+
+    _EXT_VERSION = "1.0"
+    """Plasters and the like for finishing Gypsum Sheet and Board"""
+    pass
+
+
+class ProfilesV1(BaseOpenEpdHierarchicalSpec):
+    """Profiles performance specification."""
+
+    _EXT_VERSION = "1.0"
+
+    pass
+
+
+class UnknownV1(BaseOpenEpdHierarchicalSpec):
+    """Unknown performance specification."""
+
+    _EXT_VERSION = "1.0"
+
+    pass
+
+
+class ZincV1(BaseOpenEpdHierarchicalSpec):
+    """Zinc performance specification."""
+
+    _EXT_VERSION = "1.0"
+
+    pass
+
+
+class OtherMaterialsV1(BaseOpenEpdHierarchicalSpec):
+    """Other materials performance specification."""
+
+    _EXT_VERSION = "1.0"
+
+    # Nested specs:
+    TransportationInfrastructure: TransportationInfrastructureV1 | None = None
+    Unsupported: UnsupportedV1 | None = None
+    Copper: CopperV1 | None = None
+    Earthwork: EarthworkV1 | None = None
+    ExteriorSunControlDevices: ExteriorSunControlDevicesV1 | None = None
+    GypsumFinishingCompounds: GypsumFinishingCompoundsV1 | None = None
+    Profiles: ProfilesV1 | None = None
+    Unknown: UnknownV1 | None = None
+    Zinc: ZincV1 | None = None
