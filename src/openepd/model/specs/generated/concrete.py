a0c09137ede7d16bf7ea48712d5c57ed83a9149d
Alex Cherednichenko <alex.cherednichenko@c-change-labs.com>
2024-04-19 17:38:58 -0400

    refactor: clean up Concrete documentation
    asana: [Add missing documentation to pydantic models](https://app.asana.com/0/698742722983597/1206887306120203/f)

--- src/openepd/model/specs/generated/concrete.py
+++ src/openepd/model/specs/generated/concrete.py
@@ -34,19 +34,26 @@ from openepd.model.validation.quantity import (
 
 
 class CementGroutV1(BaseOpenEpdHierarchicalSpec):
-    """Cement grout performance specification."""
+    """
+    Cement grout performance specification.
+
+    Cement grouting is a slurry that is placed as a flowable liquid. It is an effective material
+    for filling and strengthening granular soils, voids in rocks, foundation underpinnings, and
+    other underground voids. Also called structural grout, these materials typically impart
+    significant compressive strength to the system.
+    """
 
     _EXT_VERSION = "1.0"
 
 
 class ConcretePavingV1(BaseOpenEpdHierarchicalSpec):
-    """Concrete paving performance specification."""
+    """Concrete paving."""
 
     _EXT_VERSION = "1.0"
 
     # Own fields:
     flexion_strength: PressureMPaStr | None = pyd.Field(
-        default=None, description="Paving flexion strength", example="30 MPa"
+        default=None, description="Concrete flexural strength.", example="30 MPa"
     )
 
     _concrete_flexion_strength_is_quantity_validator = pyd.validator("flexion_strength", allow_reuse=True)(
@@ -55,36 +62,58 @@ class ConcretePavingV1(BaseOpenEpdHierarchicalSpec):
 
 
 class FlowableFillV1(BaseOpenEpdHierarchicalSpec):
-    """Flowable fill performance specification."""
+    """
+    Flowable fill performance specification.
+
+    Flowable fill is a slurry that is placed as a flowable liquid (high slump) and sets with no
+    compaction. It is often used in tight or restricted access areas where placing and compacting
+    fill is difficult. Applications include filling large voids such as abandoned underground storage
+    tanks, basements, tunnels, mines, and sewers. It can also be used as paving sub-base, bridge
+    abutment, and retaining wall backfill.
+
+    Also called Controlled Density Fill (CDF) or Controlled Low Strength Materials (CLSMs). These materials typically
+    have compressive strengths under 1200 psi.
+    """
 
     _EXT_VERSION = "1.0"
 
 
 class OilPatchV1(BaseOpenEpdHierarchicalSpec):
-    """Oil patch performance specification."""
+    """
+    Petroleum Industry Cement Slurry.
+
+    Concretes for use in creation, maintenance, and decommissioning of petroleum extraction wells and similar
+    applications. Includes foamed cement; often called cement in the drilling industry. Differs from
+    flowable fill and grout in that it contains no sand or other aggregates.
+    """
 
     _EXT_VERSION = "1.0"
 
 
 class ReadyMixV1(BaseOpenEpdHierarchicalSpec):
-    """Concretes that are mixed just before use, and then poured on-site into forms."""
+    """Concretes to be mixed and then poured on-site."""
 
     _EXT_VERSION = "1.0"
 
 
 class ShotcreteV1(BaseOpenEpdHierarchicalSpec):
-    """Shotcrete performance specification."""
+    """Concretes sprayed on a target."""
 
     _EXT_VERSION = "1.0"
 
 
 class ConcreteV1(BaseOpenEpdHierarchicalSpec):
-    """Concrete performance specification."""
+    """
+    Concrete.
+
+    A composite material composed of fine and coarse aggregate bonded together with a fluid cement (cement paste) that
+    hardens over time.
+    """
 
     _EXT_VERSION = "1.0"
 
     # Own fields:
-    lightweight: bool | None = pyd.Field(default=None, description="Lightweight", example=True)
+    lightweight: bool | None = pyd.Field(default=None, description="Product is lightweight", example=True)
 
     strength_28d: PressureMPaStr | None = pyd.Field(
         default=None, description="Concrete strength after 28 days", example="1 MPa"
@@ -95,14 +124,16 @@ class ConcreteV1(BaseOpenEpdHierarchicalSpec):
         example="30 MPa",
     )
     strength_other_d: Literal[3, 7, 14, 42, 56, 72, 96, 120] | None = pyd.Field(
-        default=None, description="Test Day for the Late Strength", example=42
+        default=None, description="Test Day for strength_other", example=42
     )
 
     slump: LengthInchStr | None = pyd.Field(default=None, description="", example="2 in")
     min_slump: LengthInchStr | None = pyd.Field(default=None, description="Minimum test slump", example="2 in")
     max_slump: LengthInchStr | None = pyd.Field(default=None, description="", example="2 in")
 
-    min_pipeline_size: LengthMmStr | None = pyd.Field(default=None, description="", example="200 mm")
+    min_pipeline_size: LengthMmStr | None = pyd.Field(
+        default=None, description="Minimum pipeline size", example="200 mm"
+    )
     w_c_ratio: RatioFloat | None = pyd.Field(
         default=None, description="Ratio of water to cement", example=0.5, ge=0, le=1
     )
@@ -114,13 +145,27 @@ class ConcreteV1(BaseOpenEpdHierarchicalSpec):
     finishable: bool | None = pyd.Field(default=None, description="Finishable", example=True)
     fiber_reinforced: bool | None = pyd.Field(default=None, description="fiber_reinforced", example=True)
 
-    cementitious: Cementitious | None = pyd.Field(default=None, description="")
+    cementitious: Cementitious | None = pyd.Field(
+        default=None, description="List of cementitious materials, and proportion by mass"
+    )
 
-    aggregate_size_max: LengthMmStr | None = pyd.Field(default=None, description="", example="0.0254 m")
-    cement_content: MassKgStr | None = pyd.Field(default=None, description="", example="1 kg")
-    aci_exposure_classes: list[AciExposureClass] | None = pyd.Field(default=None, description="", example=["aci.F0"])
-    csa_exposure_classes: list[CsaExposureClass] | None = pyd.Field(default=None, description="", example=["csa.C-2"])
-    en_exposure_classes: list[EnExposureClass] | None = pyd.Field(default=None, description="", example=["en206.X0"])
+    aggregate_size_max: LengthMmStr | None = pyd.Field(
+        default=None,
+        description="The smallest sieve size for which the entire amount of aggregate is able to pass. "
+        "Parameter describes diameter of aggregate",
+        example="8 mm",
+    )
+    cement_content: MassKgStr | None = pyd.Field(default=None, example="1 kg")
+
+    aci_exposure_classes: list[AciExposureClass] | None = pyd.Field(
+        default=None, description="List of ACI exposure classes", example=["aci.F0"]
+    )
+    csa_exposure_classes: list[CsaExposureClass] | None = pyd.Field(
+        default=None, description="List of CSA exposure classes", example=["csa.C-2"]
+    )
+    en_exposure_classes: list[EnExposureClass] | None = pyd.Field(
+        default=None, description="List of EN exposure classes", example=["en206.X0"]
+    )
     typical_application: ConcreteTypicalApplication | None = pyd.Field(default=None, description="Typical Application")
 
     _concrete_compressive_strength_28d_is_quantity_validator = pyd.validator("strength_28d", allow_reuse=True)(
