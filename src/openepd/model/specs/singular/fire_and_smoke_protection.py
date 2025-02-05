6c50445d4fdfc4d8d67fc5df62c4e1bc0f363aa1
Oleh Chyhyryn <oleg.chigirin@c-change-labs.com>
2025-02-05 17:00:02 +0200

    chore(lint): apply copyright to the whole project

--- src/openepd/model/specs/singular/fire_and_smoke_protection.py
+++ src/openepd/model/specs/singular/fire_and_smoke_protection.py
@@ -1,5 +1,5 @@
 #
-#  Copyright 2024 by C Change Labs Inc. www.c-change-labs.com
+#  Copyright 2025 by C Change Labs Inc. www.c-change-labs.com
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
