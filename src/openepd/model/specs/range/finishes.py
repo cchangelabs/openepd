4ace18bec96cd2287e90df91e7b1e39a88242b77
Oleh Chyhyryn <oleg.chigirin@c-change-labs.com>
2025-05-14 21:03:32 +0300

    feat(specs): allow additional CementBoard thicknesses
    asana: [[OpenEPD] Add thickness option to CementBoard](https://app.asana.com/1/654700433662128/project/1210066889396893/task/1210252537393829?focus=true)

--- src/openepd/model/specs/range/finishes.py
+++ src/openepd/model/specs/range/finishes.py
@@ -44,8 +44,6 @@ __all__ = (
     "WoodFlooringRangeV1",
 )
 
-# NB! This is a generated code. Do not edit it manually. Please see src/openepd/model/specs/README.md
-
 
 from openepd.compat.pydantic import pyd
 from openepd.model.common import RangeAmount, RangeFloat, RangeInt, RangeRatioFloat
@@ -375,7 +373,7 @@ class CementBoardRangeV1(BaseOpenEpdHierarchicalSpec):
     Range version.
     """
 
-    _EXT_VERSION = "1.0"
+    _EXT_VERSION = "1.1"
 
     thickness: list[CementBoardThickness] | None = pyd.Field(default=None, description="")
 
