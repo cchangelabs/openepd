68cf020de98eae663c23ca9cf354c3129697c61b
Bohdan Zahoruiko <bohdan.zahoruiko@c-change-labs.com>
2025-09-10 15:23:08 +0300

    feat: add `Vehicles` category
    asana: [Support for creating non-Construction EPDs.](https://app.asana.com/1/654700433662128/project/1205308458277257/task/1211102770298949?focus=true)

--- src/openepd/model/specs/singular/__init__.py
+++ src/openepd/model/specs/singular/__init__.py
@@ -56,7 +56,7 @@ from openepd.model.specs.singular.material_handling import MaterialHandlingV1
 from openepd.model.specs.singular.mechanical import MechanicalV1
 from openepd.model.specs.singular.mechanical_insulation import MechanicalInsulationV1
 from openepd.model.specs.singular.network_infrastructure import NetworkInfrastructureV1
-from openepd.model.specs.singular.non_construction import ChemicalsV1, ElectricityAndFuelV1
+from openepd.model.specs.singular.non_construction import ChemicalsV1, ElectricityAndFuelV1, VehiclesV1
 from openepd.model.specs.singular.openings import OpeningsV1
 from openepd.model.specs.singular.other_electrical_equipment import OtherElectricalEquipmentV1
 from openepd.model.specs.singular.other_materials import OtherMaterialsV1, TextileProductsV1
@@ -121,6 +121,7 @@ class Specs(BaseOpenEpdHierarchicalSpec):
     TextileProducts: TextileProductsV1 | None = None
     Chemicals: ChemicalsV1 | None = None
     ElectricityAndFuel: ElectricityAndFuelV1 | None = None
+    Vehicles: VehiclesV1 | None = None
 
     # historical backward-compatible specs
     concrete: ConcreteOldSpec | None = None
