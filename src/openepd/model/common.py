2fef0464ffe509ab5c6bb41b2709bd8ca178cedf
Bohdan Zahoruiko <bohdan.zahoruiko@c-change-labs.com>
2025-10-06 17:50:13 +0300

    chore: update Data URL regex
    add new regex group to capture base64 encoded value
    
    asana: [Add data urls support for product_image (openEPD API)](https://app.asana.com/1/654700433662128/project/1205308458277257/task/1211251162909335?focus=true)

--- src/openepd/model/common.py
+++ src/openepd/model/common.py
@@ -21,7 +21,7 @@ import pydantic_core
 
 from openepd.model.base import BaseOpenEpdSchema
 
-DATA_URL_REGEX = r"^data:([-\w]+\/[-+\w.]+)?(;?\w+=[-\w]+)*(;base64)?,.*$"
+DATA_URL_REGEX = r"^data:([-\w]+\/[-+\w.]+)?(;?\w+=[-\w]+)*(;base64)?,(.*)$"
 """
 Regular expression pattern for matching Data URLs.
 
