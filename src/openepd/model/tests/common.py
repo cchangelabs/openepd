a3a0ba490537c76d8f28135fbe2b8ffcd1f73d92
Bohdan Zahoruiko <bohdan.zahoruiko@c-change-labs.com>
2025-12-09 14:00:35 +0200

    feat: add `name` field for Generic Estimate model

--- src/openepd/model/tests/common.py
+++ src/openepd/model/tests/common.py
@@ -21,6 +21,7 @@ from openepd.model.common import DATA_URL_IMAGE_MAX_LENGTH, Amount
 GE_REQUIRED_FIELDS: Final[dict[str, Any]] = {
     "id": "0197ad82-92cf-7978-a6c8-d4964c0a3624",
     "kg_per_declared_unit": Amount(qty=12.5, unit="kg").to_serializable(exclude_unset=True),
+    "name": "Generic Estimate Sample",
 }
 """
 Required fields for Generic Estimates to be used in tests.
