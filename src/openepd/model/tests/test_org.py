66441a7f1df5f3269ece3c5e684266e4a480bdbd
Alexander Litvinenko <alexander.litvinenko@c-change-labs.com>
2026-01-09 15:41:40 +0200

    fix: allow parsing `subsidiaries` dicts into `Org` Pydantic model
    Ensure `Org` validates `subsidiaries` entries as `OrgRef` instances when provided as dicts.
    Adds tests to confirm correct coercion and validation.

--- src/openepd/model/tests/test_org.py
+++ src/openepd/model/tests/test_org.py
@@ -13,10 +13,29 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 #
-from openepd.model.org import Org
+from openepd.model.org import Org, OrgRef
 from openepd.model.tests.common import ImageFieldTestCase
 
 
 class OrgTestCase(ImageFieldTestCase):
     def test_logo(self) -> None:
         self._test_data_url_image_field(Org, "logo")
+
+    def test_org_subsidiary_is_orgref_instance(self) -> None:
+        """
+        Test that a subsidiary entry in the Org model is correctly validated as an OrgRef instance.
+
+        This test ensures that when an Org is instantiated with a subsidiary dictionary, the resulting
+        subsidiary object is an instance of OrgRef.
+        """
+        org_data: dict = {
+            "subsidiaries": [
+                {
+                    "web_domain": "epd-australasia.com",
+                    "name": "EPD Australasia",
+                    "ref": "https://openepd.cqd.io/api/orgs/epd-australasia.com",
+                }
+            ],
+        }
+        org_instance: Org = Org.model_validate(org_data)
+        self.assertIsInstance(org_instance.subsidiaries[0], OrgRef)
